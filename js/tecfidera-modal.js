9436ba49bff873db79b7c570c8475fe03116b231
Kristoffer delos Reyes <kristoffer@embassydigital.com.au>
2017-10-20 17:39:02 +0800

    Added function to modal.js and updated restyling of page components

--- js/tecfidera-modal.js
+++ js/tecfidera-modal.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
-	$('#homeModal').modal('show'); 
-});
+	$('#homeModal').modal('show');
 
+	$( "ul.desktop-footer-links li:nth-child(2), ul.mobile-footer-links li:nth-child(2)" ).click(function() {
+		$('#tecFideraInfoModal').modal('show');
+		$('#aboutBiogenModal').modal('hide');
+	});
+
+	$( "ul.desktop-footer-links li:nth-child(3), ul.mobile-footer-links li:nth-child(3)" ).click(function() {
+		$('#aboutBiogenModal').modal('show');
+		$('#tecFideraInfoModal').modal('hide');
+	});
+
+	$("ul.desktop-footer-links, ul.mobile-footer-links").hover(function() {
+			 $(this).css('cursor','pointer');
+	 });
+});
