f2a772486419aaf8b1adc74d6617bd26cb9557f4
Kristoffer delos Reyes <kristoffer@embassydigital.com.au>
2017-11-02 19:47:36 +0800

    applied changes to contents of content pages mobile view, updated modal pop up and script

--- js/tecfidera-modal.js
+++ js/tecfidera-modal.js
@@ -2,13 +2,18 @@ $(document).ready(function(){
 	$('#homeModal').modal('show');
 
 	$( "ul.desktop-footer-links li:nth-child(2), ul.mobile-footer-links li:nth-child(2)" ).click(function() {
+		$("ul.desktop-footer-links li:nth-child(3), ul.mobile-footer-links li:nth-child(3)").removeClass("modal-activelink");
 		$('#tecFideraInfoModal').modal('show');
 		$('#aboutBiogenModal').modal('hide');
+		$(this).addClass("modal-activelink");
+
 	});
 
 	$( "ul.desktop-footer-links li:nth-child(3), ul.mobile-footer-links li:nth-child(3)" ).click(function() {
+		$("ul.desktop-footer-links li, ul.mobile-footer-links li:nth-child(2)").removeClass("modal-activelink");
 		$('#aboutBiogenModal').modal('show');
 		$('#tecFideraInfoModal').modal('hide');
+		$(this).addClass("modal-activelink");
 	});
 
 	$("ul.desktop-footer-links, ul.mobile-footer-links").hover(function() {
